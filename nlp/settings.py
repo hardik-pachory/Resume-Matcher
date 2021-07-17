c810456ace8bf32c68340513da1e23a25d76e00a
Hardik Pachory <84628216+hardik-pachory@users.noreply.github.com>
2021-07-17 07:37:08 +0530

    Update settings.py

--- nlp/settings.py
+++ nlp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&g#j97)ah&n-^b2-&ezwh3ss4-ioa6#o2-tvqkp+v=+t2!jx7i'
+SECRET_KEY = '' #Here will be your own Django secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
